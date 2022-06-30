8a132b477dbf24c1d496c6188783e1cb6dce11f7
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-01 02:33:31 +0400

    Fixed all the colors

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 const menuButton = document.querySelector('.menuBtn')
 const sideMenu = document.querySelector('.sidebar')
 const infoBar = document.querySelector('.info-bar')
-const header = document.querySelector('header')
+const header = document.querySelector('.header-container')
+
+
+//Show only information bar when page is loaded
+window.onload = () => {
+    sideMenu.classList.toggle('toggle')
+    header.classList.toggle('clicked')
+};
 
 //Add an event listen to the button to hide sidebar when clicked
 menuButton.addEventListener('click', () => {
     sideMenu.classList.toggle('toggle')
     header.classList.toggle('clicked')
+    menuButton.classList.toggle('clicked')
+
+    if (menuButton.innerHTML === "Menu") {
+        menuButton.innerHTML = "Close"
+    } else {
+        menuButton.innerHTML = "Menu"
+    }
 })
